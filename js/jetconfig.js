bcadf8732c5aba3a5c9e895344c615b1f37414ff
Don M <donsmoore@gmail.com>
2026-01-05 05:39:16 -0600

    Fine-tune A-10 cannons: 1m up (y: -5.7→-4.7) and 1m more out (x: ±14.0→±15.0)

--- js/jetconfig.js
+++ js/jetconfig.js
@@ -39,8 +39,8 @@ export const JET_CONFIGS = {
             useNamedObjects: false,
             // Offset from plane center (underwing pylons)
             // X: left/right, Y: up/down, Z: forward/back
-            leftOffset: { x: -14.0, y: -5.7, z: 1.0 },  // Left outer pylon: 3m more out, 2m more down
-            rightOffset: { x: 14.0, y: -5.7, z: 1.0 }   // Right outer pylon: 3m more out, 2m more down
+            leftOffset: { x: -15.0, y: -4.7, z: 1.0 },  // Left outer pylon: 1m more out, 1m up
+            rightOffset: { x: 15.0, y: -4.7, z: 1.0 }   // Right outer pylon: 1m more out, 1m up
         },
         stats: {
             speed: 0.85,  // Slightly slower than F-16
