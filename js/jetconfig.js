079d34954b9ce32e6376f6c4033f3e0c4bb86765
Don M <donsmoore@gmail.com>
2026-01-06 04:30:42 -0600

    Move A-10 cannons 3m inward toward centerline (x: ±15.0→±12.0)

--- js/jetconfig.js
+++ js/jetconfig.js
@@ -39,8 +39,8 @@ export const JET_CONFIGS = {
             useNamedObjects: false,
             // Offset from plane center (underwing pylons)
             // X: left/right, Y: up/down, Z: forward/back
-            leftOffset: { x: -15.0, y: -4.7, z: 1.0 },  // Left outer pylon: 1m more out, 1m up
-            rightOffset: { x: 15.0, y: -4.7, z: 1.0 }   // Right outer pylon: 1m more out, 1m up
+            leftOffset: { x: -12.0, y: -4.7, z: 1.0 },  // Left pylon: 3m closer to centerline
+            rightOffset: { x: 12.0, y: -4.7, z: 1.0 }   // Right pylon: 3m closer to centerline
         },
         stats: {
             speed: 0.85,  // Slightly slower than F-16
